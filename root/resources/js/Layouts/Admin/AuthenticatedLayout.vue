8d96fa7f11aa132e1cd775e45bbe4c1d6a705d64
IwtSou <iwata099@gmail.com>
2025-06-29 22:00:29 +0900

    修正

--- root/resources/js/Layouts/Admin/AuthenticatedLayout.vue
+++ root/resources/js/Layouts/Admin/AuthenticatedLayout.vue
@@ -22,7 +22,7 @@ const showingNavigationDropdown = ref(false);
                         <div class="flex">
                             <!-- Logo -->
                             <div class="flex shrink-0 items-center">
-                                <Link :href="route('tasks.index')">
+                                <Link :href="route('admin.tasks.index')">
                                     <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
                                 </Link>
                             </div>
--- root/routes/admin/admin.php
+++ root/routes/admin/admin.php
@@ -3,22 +3,23 @@
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\TaskController;
 use App\Http\Controllers\Admin\UserController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 
 Route::middleware('auth:admin')
     ->prefix('admin')
     ->name('admin.')
     ->group(function() {
-        Route::prefix('tasks')->name('tasks.')->controller(TaskController::class)->group(function() {
-            Route::get('index', 'index')->name('index');
-            Route::get('create', 'create')->name('create');
-            Route::post('store', 'store')->name('store');
-            Route::put('update/{task}', 'update')->name('update');
-            Route::delete('destroy/{task}', 'destroy')->name('destroy');
-        });
+        Route::prefix('tasks')
+            ->name('tasks.')
+            ->controller(TaskController::class)
+            ->group(function() {
+                Route::get('/', 'index')->name('index');
+                Route::get('create', 'create')->name('create');
+                Route::post('store', 'store')->name('store');
+                Route::put('update/{task}', 'update')->name('update');
+                Route::delete('destroy/{task}', 'destroy')->name('destroy');
+            });
         Route::prefix('user')
             ->name('users.')
             ->controller(UserController::class)
@@ -27,8 +28,7 @@ Route::middleware('auth:admin')
                 Route::get('create', 'create')->name('create');
                 Route::post('store', 'store')->name('store');
                 Route::put('update/{user}', 'update')->name('update');
-            Route::delete('destroy/{user}', 'destroy')->name('destroy');
-
+                Route::delete('destroy/{user}', 'destroy')->name('destroy');
             });
     });
 
